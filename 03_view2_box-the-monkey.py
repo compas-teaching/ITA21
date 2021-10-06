056568439a81cc7584d66dd8c49594b778614053
brgcode <vanmelet@ethz.ch>
2021-10-06 12:07:14 +0200

    comments and typos

--- 03_view2_box-the-monkey.py
+++ 03_view2_box-the-monkey.py
@@ -2,14 +2,19 @@ import compas
 from compas.geometry import Box, Scale
 from compas_view2.app import App
 
+# load the mesh data
 mesh = compas.json_load('monkey.json')
 
+# add a box
+# and give the monkey some breathing room
 box = Box.from_bounding_box(mesh.bounding_box())
 box.transform(Scale.from_factors([1.5, 1.5, 1.5]))
 
+# visualize the mesh and the box with the viewer
 viewer = App()
 viewer.add(mesh)
 viewer.add(box, opacity=0.5)
 viewer.show()
 
+# export the moonkey in the box
 compas.json_dump([mesh, box], 'monkey-in-box.json')
--- 04_rhino_monkey-in-box.py
+++ 04_rhino_monkey-in-box.py
@@ -1,11 +1,18 @@
 import compas
+import compas_rhino
 from compas_rhino.artists import MeshArtist
 from compas_rhino.artists import BoxArtist
 
-mesh, box = compas.json_load('monkey-in-box.json')
+# clear the objects in the Rhino scene
+compas_rhino.clear()
 
+# load the mesh and box data
+mesh, box = compas.json_load('/Users/vanmelet/Code/ITA21/monkey-in-box.json')
+
+# viz the mesh
 artist = MeshArtist(mesh)
 artist.draw_mesh()
 
-artist = BoxArtist(mesh)
-artist.draw_mesh()
+# viz the box
+artist = BoxArtist(box)
+artist.draw()
