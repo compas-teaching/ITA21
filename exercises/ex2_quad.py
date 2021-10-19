fe3ab240b1f2bfa9990bd6fbe2f54ccf988bd644
duchaoyu <duch@student.ethz.ch>
2021-10-20 00:08:40 +0200

    add exercise 1 answer; add data for exercise 2; add exercise 2
    f

---
+++ exercises/ex2_quad.py
@@ -0,0 +1,40 @@
+import os
+import random
+import compas
+from compas.datastructures import Mesh
+from compas_plotters import MeshPlotter
+
+# deserialization
+HERE = os.path.dirname(__file__)
+FILE = os.path.join(HERE, 'data', 'quadmesh.json')
+mesh = Mesh.from_json(FILE)
+
+# select a random edge
+start = random.choice(list(mesh.edges()))
+
+# find edge strips
+edges = mesh.edge_strip(start)
+
+# find strip faces
+faces = []
+for u, v in edges:
+    if mesh.halfedge_face(u, v) is not None:
+        faces.append(mesh.halfedge_face(u, v))
+
+# visualize the edge loops
+edgecolor = {}
+for edge in edges:
+   edgecolor[edge] = (0, 255, 0)
+edgecolor[start] = (255, 0, 0)
+
+# visualize the strip faces
+facecolor = {}
+for face in faces:
+    facecolor[face] = (255, 200, 200)
+
+# plotter
+plotter = MeshPlotter(mesh, figsize=(12, 7.5))
+plotter.draw_vertices(radius=0.03)
+plotter.draw_faces(facecolor=facecolor)
+plotter.draw_edges(keys=edges, color=edgecolor, width=2.0)
+plotter.show()
