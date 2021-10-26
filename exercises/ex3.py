0b51033039e3935022f477cd9da59637cf2bb199
duchaoyu <duch@student.ethz.ch>
2021-10-26 20:18:26 +0200

    - update ex3

--- exercises/ex3.py
+++ exercises/ex3.py
@@ -1,32 +1,33 @@
-import compas
 from compas.datastructures import Mesh
-from compas_plotters import MeshPlotter
+from compas_plotters import Plotter
 
-# deserialization
-mesh = Mesh.from_obj(compas.get('faces.obj'))
+# generate a mesh grid from meshgrid
+mesh = Mesh.from_meshgrid(nx=5, dx=2)
 
-# pick a random vertex from the mesh
-vertex = mesh.get_any_vertex()
+# choose a random face sample from the mesh
+start = mesh.face_sample()[0]
 
-# step 1: pick a face connected to the vertex
-# hint: Mesh.vertex_faces
-
-
-# Step 2: develop checkered pattern starting from the face you find in step 1
+# develop checkered pattern starting from the start face 
 # checkered pattern contains two colors where a single checker is surrounded 
 # on all four sides by a checker of a different color.
 
-# Hint1: you can set a face attribute to the start face, e.g. "is_colored": True.
-# then set attribute to the neighbour faces, e.g. "is_colored": False.
+# Hint1: you can use face attribute to store the color information. e.g. "is_colored": True / False
 # To should avoid an infitie loop, you should find a way to check the face and its neighbours only once
 
 # Hint2: Mesh.face_neighbors, Mesh.update_default_face_attribute, Mesh.face_attribute
 
-# visualization
-vertexcolor = {}
-vertexcolor[vertex] = (255, 0, 0)
+# visualization face settings
+facecolor = {}
+facecolor[start] = (1, 0.7, 0.7)
+# Hint3: visualize the faces based on the face attribute you set. 
 
-plotter = MeshPlotter(mesh, figsize=(12, 7.5))
-plotter.draw_vertices(facecolor=vertexcolor)
-plotter.draw_faces()
+# plotter
+plotter = Plotter()
+artist = plotter.add(mesh,
+                     facecolor=facecolor,
+                     sizepolicy='relative',
+                     vertexsize=0.5,
+                     )
+
+plotter.zoom_extents()
 plotter.show()
