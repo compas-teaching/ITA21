ee5479b5c129bd92f3adb0d87bd60c20ded843cc
brgcode <vanmelet@ethz.ch>
2021-11-10 12:49:21 +0100

    unsaved stuff

--- L6/030b_examples_box.py
+++ L6/030b_examples_box.py
@@ -40,7 +40,9 @@ artist = Artist(mesh, layer="ITA21::L6::FormFinding")
 vertexcolor = {vertex: (255, 0, 0) for vertex in mesh.vertices_where({'is_anchor': True})}
 
 artist.clear_layer()
-artist.draw(vertexcolor=vertexcolor)
+artist.draw_mesh(disjoint=True)
+artist.draw_vertices(color=vertexcolor)
+artist.draw_edges()
 
 # this is necessary to avoid that Rhino on Mac freezes up
 # when you use the built-in editor to run this script
