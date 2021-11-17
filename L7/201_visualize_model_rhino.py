39029a020ef471f5df28bff9c5b5b10564e36da7
Gonzalo Casas <casas@arch.ethz.ch>
2021-11-17 01:52:27 +0100

    Add some examples

---
+++ L7/201_visualize_model_rhino.py
@@ -0,0 +1,7 @@
+from compas.artists import Artist
+from compas.robots import RobotModel
+
+model = RobotModel.from_urdf_file('models/05_with_colors.urdf')
+
+artist = Artist(model)
+artist.draw_visual()
